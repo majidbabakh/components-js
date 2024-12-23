44b026c5584cbdcbab4f0d85e9d4cb111f239514
Majid <majidbabak@gmail.com>
2024-12-23 04:13:30 +0330

    wip(Button): custom validation props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,37 @@
-function Button({ children }) {
+import propTypes from 'prop-types';
+
+function Button({
+  children,
+  rounded,
+  outline,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger
+}) {
   return (
     <button className="border-2 border-slate-400  m-4 px-4 py-1">
       {children}
     </button>
   );
 }
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        'only one of primary, secondary, success, warning,danger can be true '
+      );
+    }
+  }
+};
 
 export default Button;
